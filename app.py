bd222b20ee351ab44eb2d8201dcf8f3219bb6f8e
sarthakSharma5 <64486079+sarthakSharma5@users.noreply.github.com>
2021-08-30 15:48:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import platform
+from subprocess import getstatusoutput
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+  
+@app.route("/sys")
+def get_system_info():
+  try:
+    return {"machine": platform.machine(), "version": platform.version(), "platform": platform.platform(), "system": platform.system(), "processor": platform.processor(),}
+  except:
+    return "Unable to provide system info"
+
+@app.route("/ip")
+def get_ifconfig():
+  output = None
+  try:
+    status, output = getstatusoutput("ifconfig")
+  except:
+    output = "Unable to process"
+  return "<PRE>{}</PRE>".format(output)
